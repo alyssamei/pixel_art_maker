89a10c02ea4fd7ccb35bfa3e955774d6b1bbf234
Alyssa Mei <105953444+alyssamei@users.noreply.github.com>
2022-12-05 14:34:22 -0500

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,30 @@
+function makeGrid(event) {
+    // clears table content
+    table.innerHTML = '';
+    
+    // define size and color inputs
+    const height = document.getElementById('gridHeight').value;
+    const width = document.getElementById('gridWidth').value;
+    let color = document.getElementById('gridColor').value;
+    
+    // redefies color value upon user input
+    gridColor.addEventListener('input', function() {
+        color = document.getElementById('gridColor').value;
+    });
+
+    // creates a table based on user input of height i and width j
+    for (i = 0; i < height; i++) {
+        const row = document.createElement('tr');
+        for (j = 0; j < width; j++) {
+            const cell = document.createElement('td');
+            row.appendChild(cell);
+            cell.addEventListener('click', function() {
+                cell.style.backgroundColor = color;
+            });
+        };
+        table.appendChild(row);
+    };
+    event.preventDefault();
+};
+
+submit.addEventListener('click', makeGrid);
